c5cd9507492f4097335266b5342371bf454e1229
binku <binku87@gmail.com>
2016-12-21 15:50:02 +0800

    add VariationsConfig and render a dummy template

--- product.go
+++ product.go
@@ -1,1 +1,28 @@
 package product
+
+import (
+	"errors"
+
+	"github.com/qor/admin"
+	"github.com/qor/qor"
+	"github.com/qor/qor/resource"
+)
+
+func init() {
+	admin.RegisterViewPath("github.com/qor/product/views")
+}
+
+type VariationsConfig struct {
+}
+
+func (variationsConfig *VariationsConfig) GetTemplate(context *admin.Context, metaType string) ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+func (variationsConfig *VariationsConfig) ConfigureQorMeta(metaor resource.Metaor) {
+	if meta, ok := metaor.(*admin.Meta); ok {
+		meta.Type = "product_variations"
+		meta.Valuer = func(interface{}, *qor.Context) interface{} { return "" }
+		meta.SetValuer(meta.Valuer)
+	}
+}
