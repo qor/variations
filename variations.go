e153fffc68921d55fcb5e15c35c8149fb1329be7
Jinzhu <wosmvp@gmail.com>
2017-01-10 17:23:19 +0800

    Add primary attributes metas

--- variations.go
+++ variations.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/qor/admin"
 	"github.com/qor/qor/resource"
+	"github.com/qor/qor/utils"
 )
 
 func init() {
@@ -12,6 +13,7 @@ func init() {
 }
 
 type VariationsConfig struct {
+	PrimaryAttributeMetas []*admin.Meta
 }
 
 func (variationsConfig *VariationsConfig) GetTemplate(context *admin.Context, metaType string) ([]byte, error) {
@@ -21,5 +23,13 @@ func (variationsConfig *VariationsConfig) GetTemplate(context *admin.Context, me
 func (variationsConfig *VariationsConfig) ConfigureQorMeta(metaor resource.Metaor) {
 	if meta, ok := metaor.(*admin.Meta); ok {
 		meta.Type = "product_variations"
+
+		variationsRes := meta.Resource
+		for _, meta := range variationsRes.ConvertSectionToMetas(meta.Resource.EditAttrs()) {
+			tagSettings := utils.ParseTagOption(meta.FieldStruct.Tag.Get("variations"))
+			if _, ok := tagSettings["PRIMARY"]; ok {
+				variationsConfig.PrimaryAttributeMetas = append(variationsConfig.PrimaryAttributeMetas, meta)
+			}
+		}
 	}
 }
