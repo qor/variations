7f01e3150ed53483be98bfabcd48fc9aa29023e9
Jinzhu <wosmvp@gmail.com>
2017-01-12 17:43:52 +0800

    Remove not needed EditAttrs

--- variations.go
+++ variations.go
@@ -15,18 +15,10 @@ func init() {
 type VariationsConfig struct {
 	Resource     *admin.Resource
 	PrimaryAttrs []string // primary attrs should in EditAttrs of the variation resource
-	EditAttrs    []string // edit attrs should in EditAttrs of the variation resource
 }
 
 func (variationsConfig VariationsConfig) PrimaryAttributeMetas() (metas []*admin.Meta) {
 	for _, name := range variationsConfig.PrimaryAttrs {
-		metas = append(metas, variationsConfig.Resource.GetMeta(name))
-	}
-	return
-}
-
-func (variationsConfig VariationsConfig) EditMetas() (metas []*admin.Meta) {
-	for _, name := range variationsConfig.EditAttrs {
 		metas = append(metas, variationsConfig.Resource.GetMeta(name))
 	}
 	return
@@ -43,28 +35,13 @@ func (variationsConfig *VariationsConfig) ConfigureQorMeta(metaor resource.Metao
 		variationsRes := meta.Resource
 		variationsConfig.Resource = variationsRes
 		definedPrimaryAttrs := len(variationsConfig.PrimaryAttrs) > 0
-		definedEditAttrs := len(variationsConfig.EditAttrs) > 0
 
 		for _, meta := range variationsRes.ConvertSectionToMetas(meta.Resource.EditAttrs()) {
 			tagSettings := utils.ParseTagOption(meta.FieldStruct.Tag.Get("variations"))
-			isPrimaryAttrs := false
-
 			if !definedPrimaryAttrs {
 				if _, ok := tagSettings["PRIMARY"]; ok {
 					variationsConfig.PrimaryAttrs = append(variationsConfig.PrimaryAttrs, meta.Name)
-					isPrimaryAttrs = true
 				}
-			} else {
-				for _, attr := range variationsConfig.PrimaryAttrs {
-					if meta.Name == attr {
-						isPrimaryAttrs = true
-						break
-					}
-				}
-			}
-
-			if !isPrimaryAttrs && !definedEditAttrs {
-				variationsConfig.EditAttrs = append(variationsConfig.EditAttrs, meta.Name)
 			}
 		}
 	}
